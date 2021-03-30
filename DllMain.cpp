30b3646042ffa9d2d7e2d66eea9fe83092bd5b0f
Archie <pin979xp@gmail.com>
2021-03-31 01:41:22 +0200

    Add comments
    Added comments to make it more readable.

--- DllMain.cpp
+++ DllMain.cpp
@@ -4,6 +4,7 @@
 #include <MinHook.h>
 #include <stdio.h>
 
+//MinHook bs
 #define STATUS_SUCCESS 0
 #if defined _M_X64
 #pragma comment(lib, "libMinHook-x64.lib")
@@ -11,6 +12,7 @@
 #pragma comment(lib, "libMinHook-x86.lib")
 #endif
 
+//A wrapper around GetProcAddress(), just to make getting the module a little easier and clean up the code a little.
 byte* GetFunctionAddress(const char* szModule, const char* szFunction)
 {
 	HMODULE hMod = GetModuleHandleA(szModule);
@@ -20,10 +22,13 @@ byte* GetFunctionAddress(const char* szModule, const char* szFunction)
 	return reinterpret_cast<byte*>(GetProcAddress(hMod, szFunction));
 }
 
+//Just a function prototype, used when creating the pointer to the trampoline.
 using fnQuerySystemInformation = NTSTATUS(WINAPI*)(SYSTEM_INFORMATION_CLASS, PVOID, ULONG, PULONG);
 
+//The pointer to our trampoline, which executes the stolen bytes and jumps back to the original.
 fnQuerySystemInformation oQuerySystemInformation = 0;
 
+//Our hooked function - this will be called when task manager attempts to call the NtQuerySystemInformation function.
 NTSTATUS WINAPI hkNtQuerySystemInformation(
 	SYSTEM_INFORMATION_CLASS SystemInformationClass,
 	PVOID SystemInformation,
@@ -64,7 +69,7 @@ NTSTATUS WINAPI hkNtQuerySystemInformation(
 
 DWORD WINAPI Main(LPVOID lpDLL)
 {
-	//Initialize the hooking library I'm using (MinHook)
+	//Initialize the hooking library (I'm using MinHook)
 	if (MH_Initialize() != MH_OK)
 	{
 		MessageBoxA(0, "MinHook couldn't initialize.", "Unexpected error", MB_OK | MB_ICONERROR);
@@ -72,7 +77,7 @@ DWORD WINAPI Main(LPVOID lpDLL)
 		return 0;
 	}
 
-	//Find the function required to spoof Task Manager
+	//Find the function required to spoof Task Manager & PH2
 	fnQuerySystemInformation pQuerySystemInformation = (fnQuerySystemInformation)GetFunctionAddress("ntdll.dll", "NtQuerySystemInformation");
 
 	//If we didn't find it (sad)
@@ -93,6 +98,7 @@ DWORD WINAPI Main(LPVOID lpDLL)
 }
 
 
+//This is where execution starts.
 #define DLL_EXPORT extern "C" __declspec(dllexport)
 DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
